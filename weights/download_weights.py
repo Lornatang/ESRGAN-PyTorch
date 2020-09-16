243162f20e324d345fbca50d1ab5defb27c61e84
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-16 19:08:00 +0800

    Create download_weights.py

---
+++ weights/download_weights.py
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://github.com/Lornatang/ESRGAN-PyTorch/releases/download/1.0/ESRGAN_PSNR_X4.pth
+wget https://github.com/Lornatang/ESRGAN-PyTorch/releases/download/1.0/ESRGAN_RRDB_X4.pth

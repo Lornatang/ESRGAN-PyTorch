787a6d5d424cd04ab227ff61207ee8c6591ebec3
Changyu <liuchangyu1111@gmail.com>
2020-10-15 19:08:28 +0800

    update pre-train address

--- weight/download_weights.py
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/Lornatang/ESRGAN-PyTorch/releases/download/1.0/ESRGAN_PSNR_X4.pth
-wget https://github.com/Lornatang/ESRGAN-PyTorch/releases/download/1.0/ESRGAN_RRDB_X4.pth
---
+++ weight/download_weights.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://github.com/Lornatang/ESRGAN-PyTorch/releases/download/0.1/RRDBNet_PSNR_4x.pth
+wget https://github.com/Lornatang/ESRGAN-PyTorch/releases/download/0.1/ESRGAN_4x.pth

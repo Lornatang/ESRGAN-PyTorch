82e808882da455ad2fc7473b068e2689fcb0577b
Changyu <liuchangyu1111@gmail.com>
2020-10-16 21:13:33 +0800

    Fix error

--- esrgan_pytorch/loss.py
+++ esrgan_pytorch/loss.py
@@ -74,7 +74,7 @@ class PerceptionLoss(nn.Module):
         """
         super(PerceptionLoss, self).__init__()
         model = vgg19(pretrained=True)
-        self.feature_extractor = nn.Sequential(*list(model.features.children())[:feature_layer]).eval()
+        self.features = nn.Sequential(*list(model.features.children())[:feature_layer]).eval()
         # Freeze parameters. Don't train.
         for name, param in self.features.named_parameters():
             param.requires_grad = False

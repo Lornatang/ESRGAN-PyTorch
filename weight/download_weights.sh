385e74bb922aed08ffc57bf9a6c8f81c4b0f3607
Changyu <liuchangyu1111@gmail.com>
2020-10-16 19:30:33 +0800

    Pre training models are no longer available

--- weight/download_weights.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/Lornatang/ESRGAN-PyTorch/releases/download/0.1/RRDBNet_PSNR_4x.pth
-wget https://github.com/Lornatang/ESRGAN-PyTorch/releases/download/0.1/ESRGAN_4x.pth

c4256ad93f9760a836c6b3ccba3913ec5642b340
Liu Changyu <liuchangyu1111@gmail.com>
2021-09-15 09:19:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ import torch
 import torch.backends.cudnn as cudnn
 import torch.nn as nn
 import torch.optim as optim
-from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts, MultiStepLR
+from torch.optim.lr_scheduler import CosineAnnealingLR, MultiStepLR
 from torch.utils.tensorboard import SummaryWriter
 
 from model import ContentLoss
@@ -43,32 +43,32 @@ exp_name         = "exp000"                # Experiment name.
 # ==============================================================================
 if mode == "train":
     # Configure dataset.
-    train_dir             = "data/DFO2K/train"                    # The address of the training data set.
-    valid_dir             = "data/DFO2K/valid"                    # Verify the address of the data set.
-    image_size            = 128                                   # High-resolution image size in the training data set.
-    batch_size            = 16                                    # Training data batch size.
+    train_dir             = "data/DFO2K/train"          # The address of the training dataset.
+    valid_dir             = "data/DFO2K/valid"          # Verify the address of the dataset.
+    image_size            = 128                         # High-resolution image size in the training dataset.
+    batch_size            = 16                          # Data batch size.
 
     # Configure model.
-    discriminator         = Discriminator().to(device)            # Load the discriminator model.
-    generator             = Generator().to(device)                # Load the generative model.
+    discriminator         = Discriminator().to(device)  # Load the discriminator model.
+    generator             = Generator().to(device)      # Load the generator model.
 
     # Resume training.
-    start_p_epoch         = 0                                     # The number of initial iterations of the generator training phase. When set to 0, it means incremental training.
-    start_epoch           = 0                                     # The number of initial iterations of the adversarial network training. When set to 0, it means incremental training.
-    resume                = False                                 # Set to `True` to continue training from the previous training progress.
-    resume_p_weight       = ""                                    # Restore the weight of the generative model during generator training.
-    resume_d_weight       = ""                                    # Restore the weight of the generative model during the training of the adversarial network.
-    resume_g_weight       = ""                                    # Restore the weight of the discriminator model during the training of the adversarial network.
+    start_p_epoch         = 0                           # The number of initial iterations of the generator training phase. When set to 0, it means incremental training.
+    start_epoch           = 0                           # The number of initial iterations of the adversarial training phase. When set to 0, it means incremental training.
+    resume                = False                       # Set to `True` to continue training from the previous training progress.
+    resume_p_weight       = ""                          # Restore the weight of the generator model during generator training.
+    resume_d_weight       = ""                          # Restore the weight of the generator model during the training of the adversarial network.
+    resume_g_weight       = ""                          # Restore the weight of the discriminator model during the training of the adversarial network.
 
     # Train epochs.
-    p_epochs              = 1162                                  # The total number of cycles of the generator training phase.
-    epochs                = 465                                   # The total number of cycles in the training phase of the adversarial network.
+    p_epochs              = 1162                        # The total number of cycles of the generator training phase.
+    epochs                = 465                         # The total number of cycles in the training phase of the adversarial network.
 
     # Loss function.
-    psnr_criterion        = nn.MSELoss().to(device)               # PSNR metrics.
-    pixel_criterion       = nn.L1Loss().to(device)                # Pixel loss.
-    content_criterion     = ContentLoss().to(device)              # Content loss.
-    adversarial_criterion = nn.BCELoss().to(device)               # Adversarial loss.
+    psnr_criterion        = nn.MSELoss().to(device)     # PSNR metrics.
+    pixel_criterion       = nn.L1Loss().to(device)      # Pixel loss.
+    content_criterion     = ContentLoss().to(device)    # Content loss.
+    adversarial_criterion = nn.BCELoss().to(device)     # Adversarial loss.
     # Perceptual loss function weight.
     pixel_weight          = 0.01
     content_weight        = 1.0
@@ -77,13 +77,13 @@ if mode == "train":
     # Optimizer.
     p_optimizer           = optim.Adam(generator.parameters(),     0.0002, (0.9, 0.999))  # Generate model learning rate during generator training.
     d_optimizer           = optim.Adam(discriminator.parameters(), 0.0001, (0.9, 0.999))  # Discriminator learning rate during adversarial network training.
-    g_optimizer           = optim.Adam(generator.parameters(),     0.0001, (0.9, 0.999))  # The learning rate of the generator during network training.
+    g_optimizer           = optim.Adam(generator.parameters(),     0.0001, (0.9, 0.999))  # Generator learning rate during adversarial network training.
 
     # Scheduler.
     milestones            = [epochs * 0.125, epochs * 0.250, epochs * 0.500, epochs * 0.750]
-    p_scheduler           = CosineAnnealingWarmRestarts(p_optimizer, p_epochs // 4, 1, 1e-7)  # Generate model scheduler during generator training.
-    d_scheduler           = MultiStepLR(d_optimizer, list(map(int, milestones)), 0.5)         # Identify the model scheduler during adversarial training.
-    g_scheduler           = MultiStepLR(g_optimizer, list(map(int, milestones)), 0.5)         # Generate model scheduler during adversarial training.
+    p_scheduler           = CosineAnnealingLR(p_optimizer, p_epochs // 4, 1e-7)               # Generator model scheduler during generator training.
+    d_scheduler           = MultiStepLR(d_optimizer, list(map(int, milestones)), 0.5)         # Discriminator model scheduler during adversarial training.
+    g_scheduler           = MultiStepLR(g_optimizer, list(map(int, milestones)), 0.5)         # Generator model scheduler during adversarial training.
 
     # Training log.
     writer                = SummaryWriter(os.path.join("samples",  "logs", exp_name))
@@ -95,12 +95,15 @@ if mode == "train":
 # ==============================================================================
 #                              Validate configure
 # ==============================================================================
-if mode == "validate":
-    exp_dir    = os.path.join("results", "test", exp_name)  # Additional variables.
+if mode == "valid":
+    # Additional variables.
+    exp_dir    = os.path.join("results", "test", exp_name)
 
-    model      = Generator().to(device)                     # Load the super-resolution model.
-    model_path = f"results/{exp_name}/g-best.pth"           # Model weight address.
-    lr_dir     = f"data/Set5/LRbicx4"                       # Low resolution image address.
-    sr_dir     = f"results/test/{exp_name}"                 # Super-resolution image address.
-    hr_dir     = f"data/Set5/GTmod12"                       # High resolution image address.
+    # Load model.
+    model      = Generator().to(device)
+    model_path = f"results/{exp_name}/g-best.pth"
 
+    # Test data address.
+    lr_dir     = f"data/Set5/LRbicx4"
+    sr_dir     = f"results/test/{exp_name}"
+    hr_dir     = f"data/Set5/GTmod12"

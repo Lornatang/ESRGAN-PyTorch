7a6530960de29f6e42247abbbacd363c5dbbb0bc
Lorna <liuchangyu1111@gmail.com>
2021-12-30 11:06:27 +0800

    Add files via upload

---
+++ scripts/prepare_dataset.py
@@ -0,0 +1,50 @@
+# Copyright 2021 Dakewe Biotech Corporation. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import argparse
+import os
+import shutil
+
+from PIL import Image
+from torchvision.transforms import RandomCrop
+from tqdm import tqdm
+
+
+def main() -> None:
+    image_dir = f"{args.output_dir}/{args.mode}"
+
+    if os.path.exists(image_dir):
+        shutil.rmtree(image_dir)
+    os.makedirs(image_dir)
+
+    file_names = os.listdir(args.inputs_dir)
+    for file_name in tqdm(file_names, total=len(file_names)):
+        # Use PIL to read high-resolution image
+        image = Image.open(f"{args.inputs_dir}/{file_name}").convert("RGB")
+
+        if image.width >= args.image_size and image.height >= args.image_size:
+            for i in range(10):
+                new_image = RandomCrop([args.image_size, args.image_size])(image)
+                new_image.save(f"{image_dir}/{file_name.split('.')[-2]}_{i:03d}.{file_name.split('.')[-1]}")
+    print("Data split successful.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Prepare database scripts.")
+    parser.add_argument("--inputs_dir", type=str, default="DFO2K/original", help="Path to input image directory. (Default: `DFO2K/original`)")
+    parser.add_argument("--output_dir", type=str, default="DFO2K/ESRGAN", help="Path to generator image directory. (Default: `DFO2K/ESRGAN`)")
+    parser.add_argument("--image_size", type=int, default=128, help="Low-resolution image size from raw image. (Default: 128)")
+    parser.add_argument("--mode", type=str, default="train", help="Dataset information status. (Default: `train`)")
+    args = parser.parse_args()
+
+    main()
---
+++ scripts/run.py
@@ -0,0 +1,12 @@
+import os
+
+# Prepare dataset
+os.system("python ./prepare_dataset.py --inputs_dir ../data/DFO2K/original/train --output_dir ../data/DFO2K/ESRGAN --image_size 128 --mode train")
+os.system("python ./prepare_dataset.py --inputs_dir ../data/DFO2K/original/valid --output_dir ../data/DFO2K/ESRGAN --image_size 128 --mode valid")
+
+# Create LMDB database file
+os.system("python ./create_lmdb_dataset.py --inputs_dir ../data/DFO2K/ESRGAN/train --lmdb_path ../data/train_lmdb/ESRGAN/DFO2K_HR_lmdb --upscale_factor 1")
+os.system("python ./create_lmdb_dataset.py --inputs_dir ../data/DFO2K/ESRGAN/train --lmdb_path ../data/train_lmdb/ESRGAN/DFO2K_LRbicx4_lmdb --upscale_factor 4")
+
+os.system("python ./create_lmdb_dataset.py --inputs_dir ../data/DFO2K/ESRGAN/valid --lmdb_path ../data/valid_lmdb/ESRGAN/DFO2K_HR_lmdb --upscale_factor 1")
+os.system("python ./create_lmdb_dataset.py --inputs_dir ../data/DFO2K/ESRGAN/valid --lmdb_path ../data/valid_lmdb/ESRGAN/DFO2K_LRbicx4_lmdb --upscale_factor 4")

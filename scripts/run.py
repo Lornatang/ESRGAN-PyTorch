8bb66ccae8b2f2029787d6782d7eb07ed4f10964
Lorna <liuchangyu1111@gmail.com>
2022-02-08 10:53:43 +0800

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -2,7 +2,7 @@ import os
 
 # Prepare dataset
 os.system("python ./prepare_dataset.py --images_dir ../data/DFO2K/original/train --output_dir ../data/DFO2K/ESRGAN/train --image_size 204 --step 102 --num_workers 10")
-os.system("python ./prepare_dataset.py --images_dir ../data/DFO2K/original/valid --output_dir ../data/DFO2K/ESRGAN/train --image_size 204 --step 102 --num_workers 10")
+os.system("python ./prepare_dataset.py --images_dir ../data/DFO2K/original/valid --output_dir ../data/DFO2K/ESRGAN/valid --image_size 204 --step 102 --num_workers 10")
 
 # Create LMDB database file
 os.system("python ./create_lmdb_dataset.py --images_dir ../data/DFO2K/ESRGAN/train --lmdb_path ../data/train_lmdb/ESRGAN/DFO2K_HR_lmdb --upscale_factor 1")
